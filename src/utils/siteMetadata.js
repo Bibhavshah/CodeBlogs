f8bc74f451eb73b7764fb02c780392ccaac74f89
Bibhavshah <78656387+Bibhavshah@users.noreply.github.com>
2023-10-05 19:33:45 +0530

    Update siteMetadata.js

--- src/utils/siteMetadata.js
+++ src/utils/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'http://localhost:3000', // your website URL
+  siteUrl: 'https://code-blogs-sage.vercel.app/', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
   email: 'bibhavsah2001@gmail.com',
